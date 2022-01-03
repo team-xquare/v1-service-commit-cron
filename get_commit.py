634d09c9d3a8d9bf5bb5e7541608cff9b3c1c522
hwc9169 <hwc9169@gmail.com>
2022-01-03 16:07:00 +0900

    ⚡️ :: (#1) 깃허브 커밋 수 크롤링 로직 작성 및 비동기 처리

--- get_commit.py
+++ get_commit.py
@@ -7,9 +7,11 @@ async def run(user: str) -> int:
         async with session.get(github_url) as resp:
             resp = await resp.text()
             soup = BeautifulSoup(resp, features="html.parser")
-
             object = soup.find('h2', class_='f4 text-normal mb-2')
-            resp = object.getText()
-            commit_count = int(resp.split('\n')[1].strip().replace(',', ''))
-
-            return(commit_count)
+            try:
+                resp = object.getText()
+                commit_count = int(resp.split('\n')[1].strip().replace(',', ''))
+                return(commit_count)
+            except:
+                pass
+            
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import get_commit
 import asyncio
+import time
+
+s = time.time()
 
 async def main():
-    commit_count = await get_commit.run('hwc9169')
-    print(commit_count)
+    github_ids = ["Sonchaegeon", "hwc9169", "jeongjiwoo0522", "kimxwan0319", "leeseojune53", "hwc9169", "silverbeen", "JaewonKim04"] * 10
+    fts = [asyncio.ensure_future(get_commit.run(github_id)) for github_id in github_ids]
+    r = await asyncio.gather(*fts)
+    return r
 
-asyncio.run(main())
+a = asyncio.run(main())
+e = time.time()
+print(len(a), a)
+print(e -s )
